9c18c82ff98760846ca20744067181555ffee69b
rajatcodesx <67538489+rajat698@users.noreply.github.com>
2022-11-27 17:30:55 -0700

    Team information about dialog box added

--- src/GUI.java
+++ src/GUI.java
@@ -1,51 +1,64 @@
 import java.awt.Container;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class GUI extends JFrame implements ActionListener{
 
-public class GUI extends JFrame{
-
+    JMenuBar menuBar;
+    JMenuItem loadRoster;
+    JMenuItem addAttendance;
+    JMenuItem saveRoster;
+    JMenuItem plotData;
+    JMenu file;
+    JMenuItem about;
 
     public GUI(){
         loadGUI();
     }
 
     public void loadGUI() {
+
         setTitle("CSE563 Final Project");
         setBounds(300, 90, 600, 600   );
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
 
-        // Container container = getContentPane();
-        // container.setLayout(null);
+        loadRoster = new JMenuItem("Load a Roster");
+        addAttendance = new JMenuItem("Add Attendance");
+        saveRoster = new JMenuItem("Save");
+        plotData = new JMenuItem("Plot Data");
 
-        JMenuItem loadRoster = new JMenuItem("Load a Roster");
-        JMenuItem addAttendance = new JMenuItem("Add Attendance");
-        JMenuItem saveRoster = new JMenuItem("Save");
-        JMenuItem plotData = new JMenuItem("Plot Data");
-
-        JMenu file = new JMenu("File");
+        file = new JMenu("File");
         file.add(loadRoster);
         file.add(addAttendance);
         file.add(saveRoster);
         file.add(plotData);
 
-        JMenu about = new JMenu("About");
-        JMenuBar menuBar = new JMenuBar();
+        about = new JMenuItem("About");
+        about.addActionListener(this);
+        menuBar = new JMenuBar();
         
         menuBar.add(file);
         menuBar.add(about);
 
-        add(menuBar);
-        setJMenuBar(menuBar);
+        this.add(menuBar);
+        this.setJMenuBar(menuBar);
 
-        this.setVisible(true);
+        this.setVisible(true);    
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == about) {
+            Viewer.aboutDialogBox();
+        }
+    }
 
 }
---
+++ src/Viewer.java
@@ -0,0 +1,29 @@
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JDialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Viewer {
+
+
+    public static void aboutDialogBox() {
+        
+        JFrame frame = new JFrame();
+        JDialog dialog = new JDialog(frame, "About");
+        JLabel label = new JLabel("Team Members: Rajat Yadav, Abhishek Massetty, Sahil Jambhulkar, Konark, Vinita, Harshita");
+        dialog.add(label);
+
+        dialog.setSize(900, 100);
+        dialog.setVisible(true);
+    }
+}
