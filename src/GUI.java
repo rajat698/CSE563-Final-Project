47bda26de10da769e0a446bceb60f87c2f78fe3c
rajatcodesx <67538489+rajat698@users.noreply.github.com>
2022-12-02 18:07:52 -0700

    Bar plot added

--- src/GUI.java
+++ src/GUI.java
@@ -13,8 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedHashMap;
@@ -49,24 +47,31 @@ public class GUI extends JFrame implements ActionListener{
         super.paint(g);
         if(drawPlot) {
 
-            PlotData plotData = new PlotData();
-            Iterator Iterator = plotData.data().entrySet().iterator();
+            Iterator Iterator = attendance.getDatewiseStudents().entrySet().iterator();
+            
+            System.out.println(attendance.getDatewiseStudents());
 
-            int x = 140;
-            int y = 300;
-            while (Iterator.hasNext()) {
+            if (attendance.getDatewiseStudents().entrySet().size() != 0) {
+                int x = 140;
+                int y = 300;
+                while (Iterator.hasNext()) {
 
-                Map.Entry mapElement = (Map.Entry)Iterator.next();
+                    Map.Entry mapElement = (Map.Entry)Iterator.next();
 
-                int height = Integer.parseInt((String) mapElement.getValue());
-                g.fillRect (x + 30, 500 - height * 2, 30, height * 2);
+                    int height = (int) mapElement.getValue();
 
-                g.drawString((String)(mapElement.getKey()), x, 520);
-                g.drawString((String)(mapElement.getValue()),120, 507 - height * 2);
+                    g.fillRect (x + 30, 500 - height * 2, 30, height * 2);
 
-                x = x + 100;
-                y = y - height;
+                    g.drawString((String)(mapElement.getKey()), x, 520);
+                    g.drawString(Integer.toString((int)mapElement.getValue()),120, 507 - height * 2);
 
+                    x = x + 100;
+                    y = y - height;
+
+            }
+        }
+            else {
+                g.drawString("Please add attndance first", 100, 200);
             }
 
         }
@@ -119,6 +124,7 @@ public class GUI extends JFrame implements ActionListener{
             Roster roster = new Roster();
             studentRoster = roster.loadRosterData(table);
             if(studentRoster != null) {
+                
                 rosterAdded = true;
                 panel.removeAll();
                 panel.setLayout(new BorderLayout());
--- src/PlotData.java
+++ src/PlotData.java
@@ -7,10 +7,10 @@ public class PlotData {
     public Map data() {
         Map<String, String> map = new HashMap<>();
 
-        map.put("12/10/2022", "40");
-        map.put("12/11/2022", "50");
-        map.put("12/12/2022", "68");
-        // map.put("12/13/2022", "70");
+        map.put("12/10/2022", "75");
+        map.put("12/11/2022", "54");
+        map.put("12/12/2022", "45");
+        map.put("12/13/2022", "40");
         
         return map;
 
